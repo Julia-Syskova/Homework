860fffffd76c51c56c365c10e13b5d30cbc65755
Julia <pushistik12131@mail.ru>
2022-08-11 21:38:23 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = -9;
+int b = -3;
+
+int max = 0, min = 0;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
