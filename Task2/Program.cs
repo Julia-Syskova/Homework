ace3334f0431acbff45f58029149dd7b8237b8ae
Julia <pushistik12131@mail.ru>
2022-08-11 22:48:44 +0300

    привела к общему виду

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,7 @@
-﻿int a = -9;
-int b = -3;
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
 
 int max = 0, min = 0;
 
