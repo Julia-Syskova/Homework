652977342bd434aa33d2b062afb8f166dc594ceb
Julia <pushistik12131@mail.ru>
2022-08-11 22:52:28 +0300

    привела к общему виду

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,6 +1,9 @@
-﻿int a = 22;
-int b = 3;
-int c = 9;
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
 
 int max = a;
 
@@ -8,5 +11,5 @@ if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
-Console.Write("max= ");
+Console.Write("max = ");
 Console.WriteLine(max);
