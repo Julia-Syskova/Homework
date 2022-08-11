9b2bbf26808bd7b9ee4654954c0dad571e1f8a02
Julia <pushistik12131@mail.ru>
2022-08-11 23:07:11 +0300

    add Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,7 +1,9 @@
 ﻿Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
+
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
+
 Console.Write("Введите третье число: ");
 int c = int.Parse(Console.ReadLine());
 
