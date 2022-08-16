2a4aa22ae0428557405103d18d20102c229306b4
Julia <pushistik12131@mail.ru>
2022-08-16 19:12:04 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿int chislo = Read("Введите трехзначное число: ");
+int symma = chislo.ToString().Length;
+
+if (symma < 3 || symma > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(Center(chislo));
+}
+
+int Read(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Center(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
