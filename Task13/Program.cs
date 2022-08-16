bf4fe9f4ac253bf69a91196d508338e154700a98
Julia <pushistik12131@mail.ru>
2022-08-16 19:29:48 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(chislo);
+if (number.Length > 2){
+  Console.WriteLine(number[0]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
