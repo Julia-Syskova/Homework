ebbaa731bbff8fe17fdf474f81ad3875f21c398e
Julia-Syskova <110534672+Julia-Syskova@users.noreply.github.com>
2022-08-19 17:36:54 +0300

    Delete Task13 directory

--- Task13/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите число: ");
-int chislo = Convert.ToInt32(Console.ReadLine());
-string number = Convert.ToString(chislo);
-if (number.Length > 2){
-  Console.WriteLine(number[0]);
-}
-else {
-  Console.WriteLine("третьей цифры нет");
-}
