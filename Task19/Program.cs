3d8de0760fa92bb81ce013d3498ebad47c120d6f
Julia <pushistik12131@mail.ru>
2022-08-19 17:12:23 +0300

    add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int a = chislo;
+int b = 0;
+
+while (a !=0)
+{
+    b = b * 10 + a % 10;
+    a /= 10;
+}
+
+if (chislo == b)
+{
+    Console.WriteLine("Да");
+}
+ else
+ {
+    Console.WriteLine("Нет");
+ }
+
